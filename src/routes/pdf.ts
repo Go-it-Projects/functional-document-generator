4c273d0dcb67bc252bbf031e4251c85d20b47aeb
unknown <enzo.giffoni@go-it.work>
2022-11-17 14:29:29 -0300

    Add the rest of the Doc Funcional

--- src/routes/pdf.ts
+++ src/routes/pdf.ts
@@ -3,6 +3,7 @@ import PDFDocument from 'pdfkit'
 import fs from 'fs'
 import { FastifyInstance } from "fastify";
 import { z } from 'zod'
+import doc from 'pdfkit';
 
 export async function Pdf( fastify: FastifyInstance){
    fastify.post('/functionalDocument' ,async (req, reply) => {
@@ -134,16 +135,9 @@ export async function Pdf( fastify: FastifyInstance){
     .lineTo(270, 540)
     .stroke()
   
-      row(doc, 400);
-      row(doc, 420);
-      row(doc, 440);
-      row(doc, 460);
-      row(doc, 480);
-      row(doc, 500);
-      row(doc, 520);
-  
      documentInfo.userDescription?.map(data => {
         textInRowFirst(doc, data.name, 402);
+        row(doc, 400);
      })
 
     doc
@@ -210,6 +204,163 @@ export async function Pdf( fastify: FastifyInstance){
         textInRowFirst(doc, data.name, 402);
      })
 
+     doc
+     .addPage()
+     .moveDown(2)
+     .font(BOLD_FONT)
+     .fontSize(TITLE_FONT_SIZE)
+     .text('4. Configuração do Sistema', {
+         align:'left'
+     })
+ 
+     .moveDown(1)
+     .font(DEFAULT_FONT)
+     .fontSize(DEFAULT_FONT_SIZE)
+     .text('Aqui esta um exemplo da Configuração do Sistema, o usuario deverá ter a possibilidade de fazer upload de varias mockups', {
+         align:'left'
+     })
+
+
+     .moveDown(2)
+     .font(BOLD_FONT)
+     .fontSize(TITLE_FONT_SIZE)
+     .text('5. Outros requisitos do sistema/Requisitos não funcionais', {
+         align:'left'
+     })
+ 
+     .moveDown(1)
+     .font(DEFAULT_FONT)
+     .fontSize(DEFAULT_FONT_SIZE)
+     .text('Requisitos não Funcionais: balblablablablablablabalblablablablablablabalblablablablablablabalblablablablablablabalblablablablablablabalblablablablablablabalblablablablablablabalblablablablablabla', {
+         align:'left'
+     })
+
+     .moveDown(2)
+     .font(BOLD_FONT)
+     .fontSize(TITLE_FONT_SIZE)
+     .text('6. Requisitos de relatório', {
+         align:'left'
+     })
+ 
+     .moveDown(1)
+     .font(DEFAULT_FONT)
+     .fontSize(DEFAULT_FONT_SIZE)
+     .text('Requisitos de relátorio: balblablablablablablabalblablablablablablabalblablablablablablabalblablablablablablabalblablablablablablabalblablablablablablabalblablablablablablabalblablablablablabla', {
+         align:'left'
+     })
+
+     .moveDown(2)
+     .font(BOLD_FONT)
+     .fontSize(TITLE_FONT_SIZE)
+     .text('7. Integração de relátorio', {
+         align:'left'
+     })
+ 
+     .moveDown(1)
+     .font(DEFAULT_FONT)
+     .fontSize(DEFAULT_FONT_SIZE)
+     .text('Integração de relátorio: balblablablablablablabalblablablablablablabalblablablablablablabalblablablablablablabalblablablablablablabalblablablablablablabalblablablablablablabalblablablablablabla', {
+         align:'left'
+     })
+
+     .moveDown(2)
+     .font(BOLD_FONT)
+     .fontSize(TITLE_FONT_SIZE)
+     .text('8. Tratamento de Exceções/Relatório de Erros', {
+         align:'left'
+     })
+ 
+     .moveDown(1)
+     .font(DEFAULT_FONT)
+     .fontSize(DEFAULT_FONT_SIZE)
+     .text('Relatório de erros: 123 erros encontrados', {
+         align:'left'
+     })
+
+     .moveDown(2)
+     .font(BOLD_FONT)
+     .fontSize(TITLE_FONT_SIZE)
+     .text('9. Requisitos de migração/conversão de dados', {
+         align:'left'
+     })
+ 
+     .moveDown(1)
+     .font(DEFAULT_FONT)
+     .fontSize(DEFAULT_FONT_SIZE)
+     .text('Requisitos de migração: 123 migrações e conversões', {
+         align:'left'
+     })
+
+     .moveDown(2)
+     .font(BOLD_FONT)
+     .fontSize(TITLE_FONT_SIZE)
+     .text('10. Estratégia de conversão de dados', {
+         align:'left'
+     })
+ 
+     .moveDown(1)
+     .font(DEFAULT_FONT)
+     .fontSize(DEFAULT_FONT_SIZE)
+     .text('Estratégia 1,2 e 18 serão usadas', {
+         align:'left'
+     })
+
+     .moveDown(2)
+     .font(BOLD_FONT)
+     .fontSize(TITLE_FONT_SIZE)
+     .text('11. Preparação de conversão de dados', {
+         align:'left'
+     })
+ 
+     .moveDown(1)
+     .font(DEFAULT_FONT)
+     .fontSize(DEFAULT_FONT_SIZE)
+     .text('Prepare os dados de maneira cascata', {
+         align:'left'
+     })
+
+     .moveDown(2)
+     .font(BOLD_FONT)
+     .fontSize(TITLE_FONT_SIZE)
+     .text('12. Espeficações da conversão de dados', {
+         align:'left'
+     })
+ 
+     .moveDown(1)
+     .font(DEFAULT_FONT)
+     .fontSize(DEFAULT_FONT_SIZE)
+     .text('Espeficações ficaram aqui boy', {
+         align:'left'
+     })
+
+     .moveDown(2)
+     .font(BOLD_FONT)
+     .fontSize(TITLE_FONT_SIZE)
+     .text('13. Referencias', {
+         align:'left'
+     })
+ 
+     .moveDown(1)
+     .font(DEFAULT_FONT)
+     .fontSize(DEFAULT_FONT_SIZE)
+     .text('Referencias', {
+         align:'left'
+     })
+
+     .moveDown(2)
+     .font(BOLD_FONT)
+     .fontSize(TITLE_FONT_SIZE)
+     .text('14.  Problemas em aberto', {
+         align:'left'
+     })
+ 
+     .moveDown(1)
+     .font(DEFAULT_FONT)
+     .fontSize(DEFAULT_FONT_SIZE)
+     .text('Problemas em aberto: 12 problemas', {
+         align:'left'
+     })
+
     doc.end()
    })
 }
@@ -227,7 +378,6 @@ function textInRowFirst(doc: any, text: string, heigth: any) {
     return doc
   }
   
-  
   function row(doc: any, heigth: any) {
     doc
     .lineJoin('miter')
@@ -235,5 +385,7 @@ function textInRowFirst(doc: any, text: string, heigth: any) {
       .stroke()
     return doc
   }
+
+  
 
  
